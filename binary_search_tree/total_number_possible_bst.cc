9ac93e6ab675a6f0ab74ce6f43e7ef813a1ead31
sdecoder <sde.work.inbox@gmail.com>
2016-01-27 16:12:05 -0500

    2016-01-27 16:12:05

--- binary_search_tree/total_number_possible_bst.cc
+++ binary_search_tree/total_number_possible_bst.cc
@@ -4,37 +4,32 @@
 #include <iostream>
 using namespace std;
 
-unsigned long int binomialCoeff(unsigned int n, unsigned int k)
-{
-    unsigned long int res = 1;
+unsigned long int binomialCoeff(unsigned int n, unsigned int k) {
+  unsigned long int res = 1;
 
-    // Since C(n, k) = C(n, n-k)
-    if (k > n - k)
-        k = n - k;
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k) k = n - k;
 
-    // Calculate value of [n*(n-1)*---*(n-k+1)] / [k*(k-1)*---*1]
-    for (int i = 0; i < k; ++i)
-    {
-        res *= (n - i);
-        res /= (i + 1);
-    }
+  // Calculate value of [n*(n-1)*---*(n-k+1)] / [k*(k-1)*---*1]
+  for (int i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
 
-    return res;
+  return res;
 }
 
 // A Binomial coefficient based function to find nth catalan
 // number in O(n) time
-unsigned long int catalan(unsigned int n)
-{
-    // Calculate value of 2nCn
-    unsigned long int c = binomialCoeff(2*n, n);
+unsigned long int catalan(unsigned int n) {
+  // Calculate value of 2nCn
+  unsigned long int c = binomialCoeff(2 * n, n);
 
-    // return 2nCn/(n+1)
-    return c/(n+1);
+  // return 2nCn/(n+1)
+  return c / (n + 1);
 }
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+int main(int argc, char const* argv[]) {
+  /* code */
+  return 0;
 }
