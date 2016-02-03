87d2407716726b4d9980a09fdfe588560c22ad0b
sdecoder <sde.work.inbox@gmail.com>
2016-02-03 13:32:23 -0500

    2016-02-03 13:32:23

--- bit/copy-set-bits-in-a-range.cc
+++ bit/copy-set-bits-in-a-range.cc
@@ -3,39 +3,32 @@
 // range [l, r] from y to x.
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // Copy set bits in range [l, r] from y to x.
 // Note that x is passed by reference and modified
 // by this function.
-void copySetBits(unsigned &x, unsigned y,
-                 unsigned l, unsigned r)
-{
-   // l and r must be between 1 to 32
-   // (assuming ints are stored using
-   //  32 bits)
-   if (l < 1 || r > 32)
-      return ;
- 
-   // Travers in given range
-   for (int i=l; i<=r; i++)
-   {
-       // Finff a mask (A number whose
-       // only set bit is at i'th position)
-       int mask = 1 << (i-1);
- 
-       // If i'th bit is set in y, set i'th
-       // bit in x also.
-       if (y & mask)
-          x = x | mask;
-   }
-}
- 
-// Driver code
-int main()
-{
-   unsigned x = 10, y = 13, l = 2, r = 3;
-   copySetBits(x, y, l, r);
-   cout << "Modified x is " << x;
-   return 0; 
+void copySetBits(unsigned& x, unsigned y, unsigned l, unsigned r) {
+  // l and r must be between 1 to 32
+  // (assuming ints are stored using
+  //  32 bits)
+  if (l < 1 || r > 32) return;
+
+  // Travers in given range
+  for (int i = l; i <= r; i++) {
+    // Finff a mask (A number whose
+    // only set bit is at i'th position)
+    int mask = 1 << (i - 1);
+
+    // If i'th bit is set in y, set i'th
+    // bit in x also.
+    if (y & mask) x = x | mask;
+  }
 }
 
+// Driver code
+int main() {
+  unsigned x = 10, y = 13, l = 2, r = 3;
+  copySetBits(x, y, l, r);
+  cout << "Modified x is " << x;
+  return 0;
+}
