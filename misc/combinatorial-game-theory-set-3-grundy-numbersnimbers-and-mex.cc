5e555f4b60aa3781210a3f6025161ff8312d6aa2
sdecoder <sde.work.inbox@gmail.com>
2016-02-09 12:05:21 -0500

    2016-02-09 12:05:21

--- misc/combinatorial-game-theory-set-3-grundy-numbersnimbers-and-mex.cc
+++ misc/combinatorial-game-theory-set-3-grundy-numbersnimbers-and-mex.cc
@@ -4,43 +4,36 @@
  and the player to move divides the number- 'n' with 2, 3
  or 6 and then takes the floor. If the integer becomes 0,
  it is removed. The last player to move wins.  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 // A Function to calculate Mex of all the values in
 // that set.
-int calculateMex(unordered_set<int> Set)
-{
-    int Mex = 0;
- 
-    while (Set.find(Mex) != Set.end())
-        Mex++;
- 
-    return (Mex);
+int calculateMex(unordered_set<int> Set) {
+  int Mex = 0;
+
+  while (Set.find(Mex) != Set.end()) Mex++;
+
+  return (Mex);
 }
- 
+
 // A function to Compute Grundy Number of 'n'
 // Only this function varies according to the game
-int calculateGrundy (int n)
-{
-    if (n == 0)
-        return (0);
- 
-    unordered_set<int> Set; // A Hash Table
- 
-    Set.insert(calculateGrundy(n/2));
-    Set.insert(calculateGrundy(n/3));
-    Set.insert(calculateGrundy(n/6));
- 
-    return (calculateMex(Set));
+int calculateGrundy(int n) {
+  if (n == 0) return (0);
+
+  unordered_set<int> Set; // A Hash Table
+
+  Set.insert(calculateGrundy(n / 2));
+  Set.insert(calculateGrundy(n / 3));
+  Set.insert(calculateGrundy(n / 6));
+
+  return (calculateMex(Set));
 }
- 
+
 // Driver program to test above functions
-int main()
-{
-    int n = 10;
-    printf("%d", calculateGrundy (n));
-    return (0);
+int main() {
+  int n = 10;
+  printf("%d", calculateGrundy(n));
+  return (0);
 }
-
-
