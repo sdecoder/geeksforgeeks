6402c3b1a00f30eef97096fd76caea98047744ea
sdecoder <sde.work.inbox@gmail.com>
2016-02-10 16:57:51 -0500

    2016-02-10 16:57:51

--- DynamicProgramming/largest-independent-set-problem.cc
+++ DynamicProgramming/largest-independent-set-problem.cc
@@ -3,37 +3,34 @@
 #include <stdlib.h>
 
 // A utility function to find max of two integers
-int max(int x, int y) { return (x > y) ? x : y; }
+int max(int x, int y) {
+  return (x > y) ? x : y;
+}
 
 /* A binary tree node has data, pointer to left child and a pointer to
    right child */
 struct node {
   int data;
   int liss;
-  struct node *left, *right;
+  struct node* left, *right;
 };
 
 // A memoization function returns size of the largest independent set in
 //  a given binary tree
-int LISS(struct node *root) {
-  if (root == NULL)
-    return 0;
+int LISS(struct node* root) {
+  if (root == NULL) return 0;
 
-  if (root->liss)
-    return root->liss;
+  if (root->liss) return root->liss;
 
-  if (root->left == NULL && root->right == NULL)
-    return (root->liss = 1);
+  if (root->left == NULL && root->right == NULL) return (root->liss = 1);
 
   // Calculate size excluding the current node
   int liss_excl = LISS(root->left) + LISS(root->right);
 
   // Calculate size including the current node
   int liss_incl = 1;
-  if (root->left)
-    liss_incl += LISS(root->left->left) + LISS(root->left->right);
-  if (root->right)
-    liss_incl += LISS(root->right->left) + LISS(root->right->right);
+  if (root->left) liss_incl += LISS(root->left->left) + LISS(root->left->right);
+  if (root->right) liss_incl += LISS(root->right->left) + LISS(root->right->right);
 
   // Maximum of two sizes is LISS, store it for future uses.
   root->liss = max(liss_incl, liss_excl);
@@ -42,8 +39,8 @@ int LISS(struct node *root) {
 }
 
 // A utility function to create a node
-struct node *newNode(int data) {
-  struct node *temp = (struct node *)malloc(sizeof(struct node));
+struct node* newNode(int data) {
+  struct node* temp = (struct node*)malloc(sizeof(struct node));
   temp->data = data;
   temp->left = temp->right = NULL;
   temp->liss = 0;
@@ -53,7 +50,7 @@ struct node *newNode(int data) {
 // Driver program to test above functions
 int main() {
   // Let us construct the tree given in the above diagram
-  struct node *root = newNode(20);
+  struct node* root = newNode(20);
   root->left = newNode(8);
   root->left->left = newNode(4);
   root->left->right = newNode(12);
