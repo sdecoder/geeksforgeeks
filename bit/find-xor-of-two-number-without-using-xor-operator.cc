a699a7ffa02dea4adac48de21b1860831f3cf9f0
sdecoder <sde.work.inbox@gmail.com>
2016-02-21 03:32:48 -0500

    2016-02-21 03:32:48

---
+++ bit/find-xor-of-two-number-without-using-xor-operator.cc
@@ -0,0 +1,34 @@
+// C++ program to find XOR without using ^
+#include <iostream>
+using namespace std;
+ 
+// Returns XOR of x and y
+int myXOR(int x, int y)
+{
+    int res = 0; // Initialize result
+     
+    // Assuming 32-bit Integer 
+    for (int i = 31; i >= 0; i--)                     
+    {
+       // Find current bits in x and y
+       bool b1 = x & (1 << i);
+       bool b2 = y & (1 << i);
+        
+        // If both are 1 then 0 else xor is same as OR
+        bool xoredBit = (b1 & b2) ? 0 : (b1 | b2);          
+ 
+        // Update result
+        res <<= 1;
+        res |= xoredBit;
+    }
+    return res;
+}
+ 
+// Driver program to test above function
+int main()
+{
+   int x = 3, y = 5;
+   cout << "XOR is " << myXOR(x, y);
+   return 0; 
+}
+
