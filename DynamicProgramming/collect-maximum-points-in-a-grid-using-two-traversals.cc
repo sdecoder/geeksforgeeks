760a922c739ee748714a81976a22ec8d364d0acc
sdecoder <sde.work.inbox@gmail.com>
2016-03-09 01:28:11 -0500

    2016-03-09 01:28:11

--- DynamicProgramming/collect-maximum-points-in-a-grid-using-two-traversals.cc
+++ DynamicProgramming/collect-maximum-points-in-a-grid-using-two-traversals.cc
@@ -34,20 +34,16 @@ bool isValid(int x, int y1, int y2) {
 int getMaxUtil(int arr[R][C], int mem[R][C][C], int x, int y1, int y2) {
   /*---------- BASE CASES -----------*/
   // if P1 or P2 is at an invalid cell
-  if (!isValid(x, y1, y2))
-    return INT_MIN;
+  if (!isValid(x, y1, y2)) return INT_MIN;
 
   // if both traversals reach their destinations
-  if (x == R - 1 && y1 == 0 && y2 == C - 1)
-    return arr[x][y1] + arr[x][y2];
+  if (x == R - 1 && y1 == 0 && y2 == C - 1) return arr[x][y1] + arr[x][y2];
 
   // If both traversals are at last row but not at their destination
-  if (x == R - 1)
-    return INT_MIN;
+  if (x == R - 1) return INT_MIN;
 
   // If subproblem is already solved
-  if (mem[x][y1][y2] != -1)
-    return mem[x][y1][y2];
+  if (mem[x][y1][y2] != -1) return mem[x][y1][y2];
 
   // Initialize answer for this subproblem
   int ans = INT_MIN;
@@ -88,8 +84,7 @@ int geMaxCollection(int arr[R][C]) {
 // Driver program to test above functions
 int main() {
   int arr[R][C] = {
-      {3, 6, 8, 2},   {5, 2, 4, 3},   {1, 1, 20, 10},
-      {1, 1, 20, 10}, {1, 1, 20, 10},
+      {3, 6, 8, 2}, {5, 2, 4, 3}, {1, 1, 20, 10}, {1, 1, 20, 10}, {1, 1, 20, 10},
   };
   cout << "Maximum collection is " << geMaxCollection(arr);
   return 0;
