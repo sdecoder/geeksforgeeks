5ab8761843dc4589b73124a377052d6195be5810
sdecoder <sde.work.inbox@gmail.com>
2016-03-12 14:15:30 -0500

    2016-03-12 14:15:30

--- linked-list/merge-sort-for-linked-list.cc
+++ linked-list/merge-sort-for-linked-list.cc
@@ -1,28 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* Link list node */
-struct node
-{
-    int data;
-    struct node* next;
+struct node {
+  int data;
+  struct node* next;
 };
 
 /* function prototypes */
 struct node* SortedMerge(struct node* a, struct node* b);
-void FrontBackSplit(struct node* source,
-          struct node** frontRef, struct node** backRef);
+void FrontBackSplit(struct node* source, struct node** frontRef, struct node** backRef);
 
 /* sorts the linked list by changing next pointers (not data) */
-void MergeSort(struct node** headRef)
-{
+void MergeSort(struct node** headRef) {
   struct node* head = *headRef;
   struct node* a;
   struct node* b;
 
   /* Base case -- length 0 or 1 */
-  if ((head == NULL) || (head->next == NULL))
-  {
+  if ((head == NULL) || (head->next == NULL)) {
     return;
   }
 
@@ -39,28 +35,24 @@ void MergeSort(struct node** headRef)
 
 /* See http://geeksforgeeks.org/?p=3622 for details of this
    function */
-struct node* SortedMerge(struct node* a, struct node* b)
-{
+struct node* SortedMerge(struct node* a, struct node* b) {
   struct node* result = NULL;
 
   /* Base cases */
   if (a == NULL)
-     return(b);
-  else if (b==NULL)
-     return(a);
+    return (b);
+  else if (b == NULL)
+    return (a);
 
   /* Pick either a or b, and recur */
-  if (a->data <= b->data)
-  {
-     result = a;
-     result->next = SortedMerge(a->next, b);
+  if (a->data <= b->data) {
+    result = a;
+    result->next = SortedMerge(a->next, b);
+  } else {
+    result = b;
+    result->next = SortedMerge(a, b->next);
   }
-  else
-  {
-     result = b;
-     result->next = SortedMerge(a, b->next);
-  }
-  return(result);
+  return (result);
 }
 
 /* UTILITY FUNCTIONS */
@@ -68,28 +60,21 @@ struct node* SortedMerge(struct node* a, struct node* b)
      and return the two lists using the reference parameters.
      If the length is odd, the extra node should go in the front list.
      Uses the fast/slow pointer strategy.  */
-void FrontBackSplit(struct node* source,
-          struct node** frontRef, struct node** backRef)
-{
+void FrontBackSplit(struct node* source, struct node** frontRef, struct node** backRef) {
   struct node* fast;
   struct node* slow;
-  if (source==NULL || source->next==NULL)
-  {
+  if (source == NULL || source->next == NULL) {
     /* length < 2 cases */
     *frontRef = source;
     *backRef = NULL;
-  }
-  else
-  {
+  } else {
     slow = source;
     fast = source->next;
 
     /* Advance 'fast' two nodes, and advance 'slow' one node */
-    while (fast != NULL)
-    {
+    while (fast != NULL) {
       fast = fast->next;
-      if (fast != NULL)
-      {
+      if (fast != NULL) {
         slow = slow->next;
         fast = fast->next;
       }
@@ -104,35 +89,30 @@ void FrontBackSplit(struct node* source,
 }
 
 /* Function to print nodes in a given linked list */
-void printList(struct node *node)
-{
-  while(node!=NULL)
-  {
-   printf("%d ", node->data);
-   node = node->next;
+void printList(struct node* node) {
+  while (node != NULL) {
+    printf("%d ", node->data);
+    node = node->next;
   }
 }
 
 /* Function to insert a node at the beginging of the linked list */
-void push(struct node** head_ref, int new_data)
-{
+void push(struct node** head_ref, int new_data) {
   /* allocate node */
-  struct node* new_node =
-            (struct node*) malloc(sizeof(struct node));
+  struct node* new_node = (struct node*)malloc(sizeof(struct node));
 
   /* put in the data  */
-  new_node->data  = new_data;
+  new_node->data = new_data;
 
   /* link the old list off the new node */
   new_node->next = (*head_ref);
 
   /* move the head to point to the new node */
-  (*head_ref)    = new_node;
+  (*head_ref) = new_node;
 }
 
 /* Drier program to test above functions*/
-int main()
-{
+int main() {
   /* Start with the empty list */
   struct node* res = NULL;
   struct node* a = NULL;
