c16654522a9e5cd9eb4418887b56d76d03e74af9
sdecoder <sde.work.inbox@gmail.com>
2016-03-15 08:12:48 -0400

    2016-03-15 08:12:48

--- DynamicProgramming/dynamic-programming-set-18-partition-problem.cc
+++ DynamicProgramming/dynamic-programming-set-18-partition-problem.cc
@@ -5,23 +5,19 @@
 // a subset of arr[] with sun equal to given sum
 bool isSubsetSum(int arr[], int n, int sum) {
   // Base Cases
-  if (sum == 0)
-    return true;
-  if (n == 0 && sum != 0)
-    return false;
+  if (sum == 0) return true;
+  if (n == 0 && sum != 0) return false;
 
   // If last element is greater than sum, then
   // ignore it
-  if (arr[n - 1] > sum)
-    return isSubsetSum(arr, n - 1, sum);
+  if (arr[n - 1] > sum) return isSubsetSum(arr, n - 1, sum);
 
   /* else, check if sum can be obtained by any of
      the following
      (a) including the last element
      (b) excluding the last element
   */
-  return isSubsetSum(arr, n - 1, sum) ||
-         isSubsetSum(arr, n - 1, sum - arr[n - 1]);
+  return isSubsetSum(arr, n - 1, sum) || isSubsetSum(arr, n - 1, sum - arr[n - 1]);
 }
 
 // Returns true if arr[] can be partitioned in two
@@ -29,13 +25,11 @@ bool isSubsetSum(int arr[], int n, int sum) {
 bool findPartiion(int arr[], int n) {
   // Calculate sum of the elements in array
   int sum = 0;
-  for (int i = 0; i < n; i++)
-    sum += arr[i];
+  for (int i = 0; i < n; i++) sum += arr[i];
 
   // If sum is odd, there cannot be two subsets
   // with equal sum
-  if (sum % 2 != 0)
-    return false;
+  if (sum % 2 != 0) return false;
 
   // Find if there is subset with sum equal to
   // half of total sum
@@ -47,10 +41,12 @@ int main() {
   int arr[] = {3, 1, 5, 9, 12};
   int n = sizeof(arr) / sizeof(arr[0]);
   if (findPartiion(arr, n) == true)
-    printf("Can be divided into two subsets "
-           "of equal sum");
+    printf(
+        "Can be divided into two subsets "
+        "of equal sum");
   else
-    printf("Can not be divided into two subsets"
-           " of equal sum");
+    printf(
+        "Can not be divided into two subsets"
+        " of equal sum");
   return 0;
 }
