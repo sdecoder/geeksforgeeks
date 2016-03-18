5c702563acd2e2b9d3deb5a8d2d54caa31df21ca
sdecoder <sde.work.inbox@gmail.com>
2016-03-18 09:34:20 -0400

    2016-03-18 09:34:20

--- array/a-product-array-puzzle.cc
+++ array/a-product-array-puzzle.cc
@@ -1,37 +1,31 @@
-void productArray(int arr[], int n)
-{
+void productArray(int arr[], int n) {
   int i, temp = 1;
- 
+
   /* Allocate memory for the product array */
-  int *prod = (int *)malloc(sizeof(int)*n);
- 
+  int* prod = (int*)malloc(sizeof(int) * n);
+
   /* Initialize the product array as 1 */
   memset(prod, 1, n);
- 
+
   /* In this loop, temp variable contains product of
     elements on left side excluding arr[i] */
-  for(i=0; i<n; i++)
-  {
+  for (i = 0; i < n; i++) {
     prod[i] = temp;
     temp *= arr[i];
   }
- 
+
   /* Initialize temp to 1 for product on right side */
   temp = 1;
- 
+
   /* In this loop, temp variable contains product of
     elements on right side excluding arr[i] */
-  for(i= n-1; i>=0; i--)
-  {
+  for (i = n - 1; i >= 0; i--) {
     prod[i] *= temp;
     temp *= arr[i];
   }
- 
+
   /* print the constructed prod array */
-  for (i=0; i<n; i++)
-    printf("%d ", prod[i]);
- 
+  for (i = 0; i < n; i++) printf("%d ", prod[i]);
+
   return;
 }
-
-
