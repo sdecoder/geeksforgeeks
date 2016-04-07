731ee65cc916efd3b8d166b8bc66c3f4eff94f1e
sdecoder <sde.work.inbox@gmail.com>
2016-04-07 18:18:29 -0400

    2016-04-07 18:18:29

--- array/count-possible-groups-size-2-3-sum-multiple-3.cc
+++ array/count-possible-groups-size-2-3-sum-multiple-3.cc
@@ -1,49 +1,44 @@
-#include<stdio.h>
- 
+#include <stdio.h>
+
 // Returns count of all possible groups that can be formed from elements
 // of a[].
-int findgroups(int arr[], int n)
-{
-    // Create an array C[3] to store counts of elements with remainder
-    // 0, 1 and 2.  c[i] would store count of elements with remainder i
-    int c[3] = {0}, i;
- 
-    int res = 0; // To store the result
- 
-    // Count elements with remainder 0, 1 and 2
-    for (i=0; i<n; i++)
-        c[arr[i]%3]++;
- 
-    // Case 3.a: Count groups of size 2 from 0 remainder elements
-    res += ((c[0]*(c[0]-1))>>1);
- 
-    // Case 3.b: Count groups of size 2 with one element with 1
-    // remainder and other with 2 remainder
-    res += c[1] * c[2];
- 
-    // Case 4.a: Count groups of size 3 with all 0 remainder elements
-    res += (c[0] * (c[0]-1) * (c[0]-2))/6;
- 
-    // Case 4.b: Count groups of size 3 with all 1 remainder elements
-    res += (c[1] * (c[1]-1) * (c[1]-2))/6;
- 
-    // Case 4.c: Count groups of size 3 with all 2 remainder elements
-    res += ((c[2]*(c[2]-1)*(c[2]-2))/6);
- 
-    // Case 4.c: Count groups of size 3 with different remainders
-    res += c[0]*c[1]*c[2];
- 
-    // Return total count stored in res
-    return res;
+int findgroups(int arr[], int n) {
+  // Create an array C[3] to store counts of elements with remainder
+  // 0, 1 and 2.  c[i] would store count of elements with remainder i
+  int c[3] = {0}, i;
+
+  int res = 0; // To store the result
+
+  // Count elements with remainder 0, 1 and 2
+  for (i = 0; i < n; i++) c[arr[i] % 3]++;
+
+  // Case 3.a: Count groups of size 2 from 0 remainder elements
+  res += ((c[0] * (c[0] - 1)) >> 1);
+
+  // Case 3.b: Count groups of size 2 with one element with 1
+  // remainder and other with 2 remainder
+  res += c[1] * c[2];
+
+  // Case 4.a: Count groups of size 3 with all 0 remainder elements
+  res += (c[0] * (c[0] - 1) * (c[0] - 2)) / 6;
+
+  // Case 4.b: Count groups of size 3 with all 1 remainder elements
+  res += (c[1] * (c[1] - 1) * (c[1] - 2)) / 6;
+
+  // Case 4.c: Count groups of size 3 with all 2 remainder elements
+  res += ((c[2] * (c[2] - 1) * (c[2] - 2)) / 6);
+
+  // Case 4.c: Count groups of size 3 with different remainders
+  res += c[0] * c[1] * c[2];
+
+  // Return total count stored in res
+  return res;
 }
- 
+
 // Driver program to test above functions
-int main()
-{
-    int arr[] = {3, 6, 7, 2, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printf("Required number of groups are %d\n", findgroups(arr,n));
-    return 0;
+int main() {
+  int arr[] = {3, 6, 7, 2, 9};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  printf("Required number of groups are %d\n", findgroups(arr, n));
+  return 0;
 }
-
-
