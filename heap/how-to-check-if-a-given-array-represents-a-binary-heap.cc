41d34f5e84052c64a9b874e0961ed1dffbfcce62
sdecoder <sde.work.inbox@gmail.com>
2016-04-07 20:02:51 -0400

    2016-04-07 20:02:51

--- heap/how-to-check-if-a-given-array-represents-a-binary-heap.cc
+++ heap/how-to-check-if-a-given-array-represents-a-binary-heap.cc
@@ -2,35 +2,28 @@
 // represents a max-heap or not
 #include <stdio.h>
 #include <limits.h>
- 
+
 // Returns true if arr[i..n-1] represents a
 // max-heap
-bool isHeap(int arr[],  int n)
-{
-    // Start from root and go till the last internal
-    // node
-    for (int i=0; i<=(n-2)/2; i++)
-    {
-        // If left child is greater, return false
-        if (arr[2*i +1] > arr[i])
-                return false;
- 
-        // If right child is greater, return false
-        if (arr[2*i+2] > arr[i])
-                return false;
-    }
-    return true;
+bool isHeap(int arr[], int n) {
+  // Start from root and go till the last internal
+  // node
+  for (int i = 0; i <= (n - 2) / 2; i++) {
+    // If left child is greater, return false
+    if (arr[2 * i + 1] > arr[i]) return false;
+
+    // If right child is greater, return false
+    if (arr[2 * i + 2] > arr[i]) return false;
+  }
+  return true;
 }
- 
+
 // Driver program
-int main()
-{
-    int arr[] = {90, 15, 10, 7, 12, 2, 7, 3};
-    int n = sizeof(arr) / sizeof(int);
- 
-    isHeap(arr, n)? printf("Yes"): printf("No");
- 
-    return 0;
+int main() {
+  int arr[] = {90, 15, 10, 7, 12, 2, 7, 3};
+  int n = sizeof(arr) / sizeof(int);
+
+  isHeap(arr, n) ? printf("Yes") : printf("No");
+
+  return 0;
 }
-
-
