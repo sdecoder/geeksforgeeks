6b6465d84bd8d26cb9531521d7798c98c730fc0b
sdecoder <sde.work.inbox@gmail.com>
2016-05-01 11:24:25 -0400

    2016-05-01 11:24:25

--- string/permutations-of-a-given-string-using-stl.cc
+++ string/permutations-of-a-given-string-using-stl.cc
@@ -2,28 +2,24 @@
 // duplicates allowed using next_permute()
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // Function to print permutations of string str
 // using next_permute()
-void permute(string str)
-{
-    // Sort the string in lexicographically
-    // ascennding order
-    sort(str.begin(), str.end());
- 
-    // Keep printing next permutation while there
-    // is next permutation
-    do {
-       cout << str << endl;
-    } while (next_permutation(str.begin(), str.end()));
+void permute(string str) {
+  // Sort the string in lexicographically
+  // ascennding order
+  sort(str.begin(), str.end());
+
+  // Keep printing next permutation while there
+  // is next permutation
+  do {
+    cout << str << endl;
+  } while (next_permutation(str.begin(), str.end()));
 }
- 
+
 // Driver code
-int main()
-{
-    string str = "CBA";
-    permute(str);
-    return 0;
+int main() {
+  string str = "CBA";
+  permute(str);
+  return 0;
 }
-
-
