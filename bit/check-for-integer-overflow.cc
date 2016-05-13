7765d58f32e350deb08429aa260cd00c0bd6756c
sdecoder <sde.work.inbox@gmail.com>
2016-05-13 02:44:07 -0400

    2016-05-13 02:44:07

--- bit/check-for-integer-overflow.cc
+++ bit/check-for-integer-overflow.cc
@@ -1,28 +1,23 @@
-#include<stdio.h>
-#include<limits.h>
-#include<stdlib.h>
- 
-int addOvf(int* result, int a, int b)
-{
-   if( a > INT_MAX - b)
-     return -1;
-   else
-   {
-     *result = a + b;
-      return 0;
-   }
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int addOvf(int* result, int a, int b) {
+  if (a > INT_MAX - b)
+    return -1;
+  else {
+    *result = a + b;
+    return 0;
+  }
 }
- 
-int main()
-{
-  int *res = (int *)malloc(sizeof(int));
+
+int main() {
+  int* res = (int*)malloc(sizeof(int));
   int x = 2147483640;
   int y = 10;
- 
+
   printf("%d", addOvf(res, x, y));
   printf("\n %d", *res);
   getchar();
   return 0;
 }
-
-
