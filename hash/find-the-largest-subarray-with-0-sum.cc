64a9d021a2173d537f7041f635f8c7202d2fa2a9
sdecoder <sde.work.inbox@gmail.com>
2016-05-14 11:05:04 -0400

    2016-05-14 11:05:04

--- hash/find-the-largest-subarray-with-0-sum.cc
+++ hash/find-the-largest-subarray-with-0-sum.cc
@@ -15,10 +15,8 @@ int maxLen(int arr[], int n) {
     // Add current element to sum
     sum += arr[i];
 
-    if (arr[i] == 0 && max_len == 0)
-      max_len = 1;
-    if (sum == 0)
-      max_len = i + 1;
+    if (arr[i] == 0 && max_len == 0) max_len = 1;
+    if (sum == 0) max_len = i + 1;
 
     // Look for this sum in Hash table
     if (presum.find(sum) != presum.end()) {
