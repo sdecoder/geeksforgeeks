d41e7f8b81fe15b668f902ada827995394b753b5
sdecoder <sde.work.inbox@gmail.com>
2016-05-17 04:13:06 -0400

    2016-05-17 04:13:06

---
+++ graph/steiner-tree.cc
@@ -0,0 +1,9 @@
+/*
+Steiner Tree Problem
+What is Steiner Tree?
+Given a graph and a subset of vertices in the graph, a steiner tree spans though the given subset. The Steiner Tree may contain some vertices which are not in given subset but are used to connect the vertices of subset.
+
+The given set of vertices is called Terminal Vertices and other vertices that are used to construct Steiner tree are called Steiner vertices.
+
+
+*/
