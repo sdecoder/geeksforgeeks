b170b2fecf9d707d6099bf4410300e4f0fd4aa15
sdecoder <sde.work.inbox@gmail.com>
2016-05-27 19:13:38 -0400

    2016-05-27 19:13:38

--- DynamicProgramming/minimum-number-of-jumps-to-reach-end-of-a-given-array.cc
+++ DynamicProgramming/minimum-number-of-jumps-to-reach-end-of-a-given-array.cc
@@ -1,15 +1,16 @@
 #include <limits.h>
 #include <stdio.h>
 
-int min(int x, int y) { return (x < y) ? x : y; }
+int min(int x, int y) {
+  return (x < y) ? x : y;
+}
 
 // Returns minimum number of jumps to reach arr[n-1] from arr[0]
 int minJumps(int arr[], int n) {
-  int *jumps = new int[n]; // jumps[n-1] will hold the result
+  int* jumps = new int[n]; // jumps[n-1] will hold the result
   int i, j;
 
-  if (n == 0 || arr[0] == 0)
-    return INT_MAX;
+  if (n == 0 || arr[0] == 0) return INT_MAX;
 
   jumps[0] = 0;
 
