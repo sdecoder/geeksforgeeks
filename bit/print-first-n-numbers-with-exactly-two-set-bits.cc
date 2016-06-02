37669d5769212453711dda0d1eb7e958c33ae57e
sdecoder <sde.work.inbox@gmail.com>
2016-06-01 23:54:28 -0400

    2016-06-01 23:54:28

--- bit/print-first-n-numbers-with-exactly-two-set-bits.cc
+++ bit/print-first-n-numbers-with-exactly-two-set-bits.cc
@@ -2,44 +2,39 @@
 // with exactly two set bits
 #include <iostream>
 using namespace std;
- 
+
 // Prints first n numbers with two set bits
-void printTwoSetBitNums(int n)
-{
-    // Initialize higher of two sets bits
-    int x = 1;
- 
-    // Keep reducing n for every number
-    // with two set bits.
-    while (n > 0)
-    {
-        // Consider all lower set bits for
-        // current higher set bit
-        int y = 0;
-        while (y < x)
-        {
-            // Print current number
-            cout << (1 << x) + (1 << y) << " ";
- 
-            // If we have found n numbers
-            n--;
-            if (n == 0)
-                return;
- 
-            // Consider next lower bit for current
-            // higher bit.
-            y++;
-        }
- 
-        // Increment higher set bit
-        x++;
+void printTwoSetBitNums(int n) {
+  // Initialize higher of two sets bits
+  int x = 1;
+
+  // Keep reducing n for every number
+  // with two set bits.
+  while (n > 0) {
+    // Consider all lower set bits for
+    // current higher set bit
+    int y = 0;
+    while (y < x) {
+      // Print current number
+      cout << (1 << x) + (1 << y) << " ";
+
+      // If we have found n numbers
+      n--;
+      if (n == 0) return;
+
+      // Consider next lower bit for current
+      // higher bit.
+      y++;
     }
+
+    // Increment higher set bit
+    x++;
+  }
 }
- 
+
 // Driver code
-int main()
-{
-    printTwoSetBitNums(4);
- 
-    return 0;
+int main() {
+  printTwoSetBitNums(4);
+
+  return 0;
 }
