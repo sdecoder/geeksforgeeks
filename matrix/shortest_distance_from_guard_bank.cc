3a692af9293137f18133f9dae842084ce04a11c5
sdecoder <sde.work.inbox@gmail.com>
2016-06-11 01:42:27 -0400

    2016-06-11 01:42:27

--- matrix/shortest_distance_from_guard_bank.cc
+++ matrix/shortest_distance_from_guard_bank.cc
@@ -8,112 +8,91 @@ using namespace std;
 #define N 5
 
 // An Data Structure for queue used in BFS
-struct queueNode
-{
-    // i, j and distance stores x and y-coordinates
-    // of a matrix cell and its distance from guard
-    // respectively
-    int i, j, distance;
+struct queueNode {
+  // i, j and distance stores x and y-coordinates
+  // of a matrix cell and its distance from guard
+  // respectively
+  int i, j, distance;
 };
 
 // These arrays are used to get row and column
 // numbers of 4 neighbors of a given cell
-int row[] = { -1, 0, 1, 0};
-int col[] = { 0, 1, 0, -1 };
+int row[] = {-1, 0, 1, 0};
+int col[] = {0, 1, 0, -1};
 
 // return true if row number and column number
 // is in range
-bool isValid(int i, int j)
-{
-    if ((i < 0 || i > M - 1) || (j < 0 || j > N - 1))
-        return false;
+bool isValid(int i, int j) {
+  if ((i < 0 || i > M - 1) || (j < 0 || j > N - 1)) return false;
 
-    return true;
+  return true;
 }
 
 // return true if current cell is an open area and its
 // distance from guard is not calculated yet
-bool isSafe(int i, int j, char matrix[][N], int output[][N])
-{
-    if (matrix[i][j] != 'O' || output[i][j] != -1)
-        return false;
+bool isSafe(int i, int j, char matrix[][N], int output[][N]) {
+  if (matrix[i][j] != 'O' || output[i][j] != -1) return false;
 
-    return true;
+  return true;
 }
 
 // Function to replace all of the O's in the matrix
 // with their shortest distance from a guard
-void findDistance(char matrix[][N])
-{
-    int output[M][N];
-    queue<queueNode> q;
+void findDistance(char matrix[][N]) {
+  int output[M][N];
+  queue<queueNode> q;
 
-    // finding Guards location and adding into queue
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            // initialize each cell as -1
-            output[i][j] = -1;
-            if (matrix[i][j] == 'G')
-            {
-                queueNode pos = {i, j, 0};
-                q.push(pos);
-                // guard has 0 distance
-                output[i][j] = 0;
-            }
-        }
+  // finding Guards location and adding into queue
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      // initialize each cell as -1
+      output[i][j] = -1;
+      if (matrix[i][j] == 'G') {
+        queueNode pos = {i, j, 0};
+        q.push(pos);
+        // guard has 0 distance
+        output[i][j] = 0;
+      }
+    }
+  }
+
+  // do till queue is empty
+  while (!q.empty()) {
+    // get the front cell in the queue and update
+    // its adjacent cells
+    queueNode curr = q.front();
+    int x = curr.i, y = curr.j, dist = curr.distance;
+
+    // do for each adjacent cell
+    for (int i = 0; i < 4; i++) {
+      // if adjacent cell is valid, has path and
+      // not visited yet, en-queue it.
+      if (isSafe(x + row[i], y + col[i], matrix, output)
+          && isValid(x + row[i], y + col[i])) {
+        output[x + row[i]][y + col[i]] = dist + 1;
+
+        queueNode pos = {x + row[i], y + col[i], dist + 1};
+        q.push(pos);
+      }
     }
 
-    // do till queue is empty
-    while (!q.empty())
-    {
-        // get the front cell in the queue and update
-        // its adjacent cells
-        queueNode curr = q.front();
-        int x = curr.i, y = curr.j, dist = curr.distance;
+    // dequeue the front cell as its distance is found
+    q.pop();
+  }
 
-        // do for each adjacent cell
-        for (int i = 0; i < 4; i++)
-        {
-            // if adjacent cell is valid, has path and
-            // not visited yet, en-queue it.
-            if (isSafe(x + row[i], y + col[i], matrix, output)
-                && isValid(x + row[i], y + col[i]))
-            {
-                output[x + row[i]][y + col[i]] = dist + 1;
-
-                queueNode pos = {x + row[i], y + col[i], dist + 1};
-                q.push(pos);
-            }
-        }
-
-        // dequeue the front cell as its distance is found
-        q.pop();
-    }
-
-    // print output matrix
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++)
-            cout << std::setw(3) << output[i][j];
-        cout << endl;
-    }
+  // print output matrix
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) cout << std::setw(3) << output[i][j];
+    cout << endl;
+  }
 }
 
 // Driver code
-int main()
-{
-    char matrix[][N] =
-    {
-        {'O', 'O', 'O', 'O', 'G'},
-        {'O', 'W', 'W', 'O', 'O'},
-        {'O', 'O', 'O', 'W', 'O'},
-        {'G', 'W', 'W', 'W', 'O'},
-        {'O', 'O', 'O', 'O', 'G'}
-    };
+int main() {
+  char matrix[][N] = {{'O', 'O', 'O', 'O', 'G'}, {'O', 'W', 'W', 'O', 'O'},
+      {'O', 'O', 'O', 'W', 'O'}, {'G', 'W', 'W', 'W', 'O'}, {'O', 'O', 'O', 'O', 'G'}};
 
-    findDistance(matrix);
+  findDistance(matrix);
 
-    return 0;
+  return 0;
 }
