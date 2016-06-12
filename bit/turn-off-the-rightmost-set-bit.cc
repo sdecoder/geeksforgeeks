a5ca769ab1221eb53f01e99b0eb950ed7285a6c0
sdecoder <sde.work.inbox@gmail.com>
2016-06-12 10:34:02 -0400

    2016-06-12 10:34:02

---
+++ bit/turn-off-the-rightmost-set-bit.cc
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+/* unsets the rightmost set bit of n and returns the result */
+int fun(unsigned int n)
+{
+  return n&(n-1);
+}    
+ 
+/* Driver program to test above function */
+int main()
+{
+  int n = 7;
+  printf("The number after unsetting the rightmost set bit %d", fun(n));
+ 
+  getchar();
+  return 0;
+}
+
+
