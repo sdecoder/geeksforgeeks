be1ca133b8cd052b4d04368fa354a48adf950709
sdecoder <sde.work.inbox@gmail.com>
2016-06-14 01:27:36 -0400

    2016-06-14 01:27:36

--- string/print-ways-break-string-bracket-form.cc
+++ string/print-ways-break-string-bracket-form.cc
@@ -3,34 +3,28 @@
 // string
 #include <iostream>
 using namespace std;
- 
+
 // find all combinations of non-overlapping
 // substrings formed by input string str
 // index – index of the next character to
 //          be processed
 // out - output string so far
-void findCombinations(string str, int index,
-                      string out)
-{
-    if (index == str.length())
-        cout << out << endl;
- 
-    for (int i = index; i < str.length(); i ++)
- 
-        // append substring formed by str[index,
-        // i] to output string
-        findCombinations(str, i + 1, out +
-          "(" + str.substr(index, i+1-index) + ")" );
-}
- 
-// Driver Code
-int main()
-{
-    // input string
-    string str = "abcd";
- 
-    findCombinations(str, 0, "");
- 
-    return 0;
+void findCombinations(string str, int index, string out) {
+  if (index == str.length()) cout << out << endl;
+
+  for (int i = index; i < str.length(); i++)
+
+    // append substring formed by str[index,
+    // i] to output string
+    findCombinations(str, i + 1, out + "(" + str.substr(index, i + 1 - index) + ")");
 }
 
+// Driver Code
+int main() {
+  // input string
+  string str = "abcd";
+
+  findCombinations(str, 0, "");
+
+  return 0;
+}
