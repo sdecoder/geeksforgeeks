7ddcf14c6cb730b862c621c74b447c1e2ea7c8a8
sdecoder <sde.work.inbox@gmail.com>
2016-06-22 16:48:42 -0400

    2016-06-22 16:48:42

--- linked-list/check-linked-list-strings-form-palindrome.cc
+++ linked-list/check-linked-list-strings-form-palindrome.cc
@@ -4,77 +4,64 @@
 using namespace std;
 
 /* Link list node */
-struct Node
-{
-    string data;
-    Node* next;
+struct Node {
+  string data;
+  Node* next;
 };
 
 // A utility function to check if str is palindrome
 // or not
-bool isPalindromeUtil(string str)
-{
-    int length = str.length();
+bool isPalindromeUtil(string str) {
+  int length = str.length();
 
-    // Match characters from beginning and
-    // end.
-    for (int i=0; i<length; i++)
-        if (str[i] != str[length-i-1])
-            return false;
+  // Match characters from beginning and
+  // end.
+  for (int i = 0; i < length; i++)
+    if (str[i] != str[length - i - 1]) return false;
 
-    return true;
+  return true;
 }
 
 // Returns true if string formed by linked
 // list is palindrome
-bool isPalindrome(Node *node)
-{
-    // Append all nodes to form a string
-    string str = "";
-    while (node != NULL)
-    {
-        str.append(node->data);
-        node = node->next;
-    }
+bool isPalindrome(Node* node) {
+  // Append all nodes to form a string
+  string str = "";
+  while (node != NULL) {
+    str.append(node->data);
+    node = node->next;
+  }
 
-    // Check if the formed string is palindrome
-    return isPalindromeUtil(str);
+  // Check if the formed string is palindrome
+  return isPalindromeUtil(str);
 }
 
 // A utility function to print a given linked list
-void printList(Node *node)
-{
-    while (node != NULL)
-    {
-        cout << node->data << " -> ";
-        node = node->next;
-    }
-    printf("NULL\n");
+void printList(Node* node) {
+  while (node != NULL) {
+    cout << node->data << " -> ";
+    node = node->next;
+  }
+  printf("NULL\n");
 }
 
 /* Function to create a new node with given data */
-Node *newNode(const char *str)
-{
-    Node *new_node = new Node;
-    new_node->data = str;
-    new_node->next = NULL;
-    return new_node;
+Node* newNode(const char* str) {
+  Node* new_node = new Node;
+  new_node->data = str;
+  new_node->next = NULL;
+  return new_node;
 }
 
 /* Driver program to test above function*/
-int main()
-{
-    Node *head = newNode("a");
-    head->next = newNode("bc");
-    head->next->next = newNode("d");
-    head->next->next->next = newNode("dcb");
-    head->next->next->next->next = newNode("a");
+int main() {
+  Node* head = newNode("a");
+  head->next = newNode("bc");
+  head->next->next = newNode("d");
+  head->next->next->next = newNode("dcb");
+  head->next->next->next->next = newNode("a");
 
-    isPalindrome(head)? printf("true\n"):
-                        printf("false\n");
+  isPalindrome(head) ? printf("true\n") : printf("false\n");
 
-    return 0;
+  return 0;
 }
-
-
-
