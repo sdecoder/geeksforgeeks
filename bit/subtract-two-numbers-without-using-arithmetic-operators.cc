8d55aedc24665c5c598782e1e571789f74d640a9
sdecoder <sde.work.inbox@gmail.com>
2016-07-02 13:28:01 -0400

    2016-07-02 13:28:01

---
+++ bit/subtract-two-numbers-without-using-arithmetic-operators.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ 
+int subtract(int x, int y)
+{
+    if (y == 0)
+        return x;
+    return subtract(x ^ y, (~x & y) << 1);
+}
+ 
+// Driver program
+int main()
+{
+    int x = 29, y = 13;
+    printf("x - y is %d", subtract(x, y));
+    return 0;
+}
+
+
