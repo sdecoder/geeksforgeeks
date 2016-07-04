f16a7a1222f10a98f5d23f21979043e34854b4e8
sdecoder <sde.work.inbox@gmail.com>
2016-07-03 23:56:31 -0400

    2016-07-03 23:56:31

--- matrix/return-previous-element-in-an-expanding-matrix.cc
+++ matrix/return-previous-element-in-an-expanding-matrix.cc
@@ -2,46 +2,40 @@
 // matrix.
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // Returns left of str in an expanding matrix of
 // a, b, c  and d.
-string findLeft(string str)
-{
-    int n = str.length();
- 
-    // Start from rightmost position
-    while (n--)
-    {
-        // If the current character is ‘b’ or ‘d’,
-        // change to ‘a’ or ‘c’ respectively and
-        // break the loop
-        if (str[n] == 'd')
-        {
-            str[n] = 'c';
-            break;
-        }
-        if (str[n] == 'b')
-        {
-            str[n] = 'a';
-            break;
-        }
- 
-        // If the current character is ‘a’ or ‘c’,
-        // change it to ‘b’ or ‘d’ respectively
-        if (str[n] == 'a')
-            str[n] = 'b';
-        else if (str[n] == 'c')
-            str[n] = 'd';
+string findLeft(string str) {
+  int n = str.length();
+
+  // Start from rightmost position
+  while (n--) {
+    // If the current character is ‘b’ or ‘d’,
+    // change to ‘a’ or ‘c’ respectively and
+    // break the loop
+    if (str[n] == 'd') {
+      str[n] = 'c';
+      break;
     }
- 
-    return str;
+    if (str[n] == 'b') {
+      str[n] = 'a';
+      break;
+    }
+
+    // If the current character is ‘a’ or ‘c’,
+    // change it to ‘b’ or ‘d’ respectively
+    if (str[n] == 'a')
+      str[n] = 'b';
+    else if (str[n] == 'c')
+      str[n] = 'd';
+  }
+
+  return str;
 }
- 
+
 // driver program to test above method
-int main()
-{
-    string str = "aacbddc";
-    cout << "Left of " << str << " is "
-         << findLeft(str);
-   return 0;
+int main() {
+  string str = "aacbddc";
+  cout << "Left of " << str << " is " << findLeft(str);
+  return 0;
 }
