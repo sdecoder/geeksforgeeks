300cd52d6c5e0e93412cedf9d9f606a3345fbce8
sdecoder <sde.work.inbox@gmail.com>
2016-07-11 13:35:49 -0400

    2016-07-11 13:35:49

--- bit/efficient-way-to-multiply-with-7.cc
+++ bit/efficient-way-to-multiply-with-7.cc
@@ -1,20 +1,17 @@
-# include<stdio.h>
- 
-int multiplyBySeven(unsigned int n)
-{  
-    /* Note the inner bracket here. This is needed 
-       because precedence of '-' operator is higher 
-       than '<<' */
-    return ((n<<3) - n);
-}
- 
-/* Driver program to test above function */
-int main()
-{
-    unsigned int n = 4;
-    printf("%u", multiplyBySeven(n));
- 
-    getchar();
-    return 0;
+#include <stdio.h>
+
+int multiplyBySeven(unsigned int n) {
+  /* Note the inner bracket here. This is needed
+     because precedence of '-' operator is higher
+     than '<<' */
+  return ((n << 3) - n);
 }
 
+/* Driver program to test above function */
+int main() {
+  unsigned int n = 4;
+  printf("%u", multiplyBySeven(n));
+
+  getchar();
+  return 0;
+}
