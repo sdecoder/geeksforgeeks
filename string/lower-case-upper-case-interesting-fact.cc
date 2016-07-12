476d874eae7dea57381e8f06168d63c317daeb71
sdecoder <sde.work.inbox@gmail.com>
2016-07-12 07:13:07 -0400

    2016-07-12 07:13:07

--- string/lower-case-upper-case-interesting-fact.cc
+++ string/lower-case-upper-case-interesting-fact.cc
@@ -1,22 +1,17 @@
 // C++ program to convert a string to uppercase
 #include <iostream>
 using namespace std;
- 
+
 // Converts a string to uppercase
-string to_upper(string &in)
-{
-    for (int i = 0; i < in.length(); i++)
-          if ('a' <= in[i] <= 'z')
-              in[i] &= ~(1 << 5);
-    return in;
+string to_upper(string& in) {
+  for (int i = 0; i < in.length(); i++)
+    if ('a' <= in[i] <= 'z') in[i] &= ~(1 << 5);
+  return in;
 }
- 
+
 // Driver code
-int main()
-{
-   string str = "geeksforgeeks";
-   cout << to_upper(str);
-   return 0;
+int main() {
+  string str = "geeksforgeeks";
+  cout << to_upper(str);
+  return 0;
 }
-
-
