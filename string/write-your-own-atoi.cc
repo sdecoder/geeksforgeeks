d7674b2e913df1531cd0cc5da4b75bb86a91e1e8
sdecoder <sde.work.inbox@gmail.com>
2016-07-15 05:56:32 -0400

    2016-07-15 05:56:32

--- string/write-your-own-atoi.cc
+++ string/write-your-own-atoi.cc
@@ -1,27 +1,22 @@
 // A simple C++ program for implementation of atoi
 #include <stdio.h>
-  
+
 // A simple atoi() function
-int myAtoi(char *str)
-{
-    int res = 0; // Initialize result
-  
-    // Iterate through all characters of input string and
-    // update result
-    for (int i = 0; str[i] != '\0'; ++i)
-        res = res*10 + str[i] - '0';
-  
-    // return result.
-    return res;
+int myAtoi(char* str) {
+  int res = 0; // Initialize result
+
+  // Iterate through all characters of input string and
+  // update result
+  for (int i = 0; str[i] != '\0'; ++i) res = res * 10 + str[i] - '0';
+
+  // return result.
+  return res;
 }
-  
+
 // Driver program to test above function
-int main()
-{
-    char str[] = "89789";
-    int val = myAtoi(str);
-    printf ("%d ", val);
-    return 0;
+int main() {
+  char str[] = "89789";
+  int val = myAtoi(str);
+  printf("%d ", val);
+  return 0;
 }
-
-
