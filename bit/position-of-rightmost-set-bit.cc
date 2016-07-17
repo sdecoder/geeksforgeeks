46cb08e84c974cf020b6310ed4f35bdb18950b60
sdecoder <sde.work.inbox@gmail.com>
2016-07-17 03:40:31 -0400

    2016-07-17 03:40:31

--- bit/position-of-rightmost-set-bit.cc
+++ bit/position-of-rightmost-set-bit.cc
@@ -1,15 +1,13 @@
-#include<stdio.h>
-#include<math.h>
- 
-unsigned int getFirstSetBitPos(int n)
-{
-   return log2(n&-n)+1;
+#include <stdio.h>
+#include <math.h>
+
+unsigned int getFirstSetBitPos(int n) {
+  return log2(n & -n) + 1;
 }
- 
-int main()
-{
-    int n = 12;
-    printf("%u", getFirstSetBitPos(n));
-    getchar();
-    return 0;
+
+int main() {
+  int n = 12;
+  printf("%u", getFirstSetBitPos(n));
+  getchar();
+  return 0;
 }
