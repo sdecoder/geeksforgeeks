87e036957b516b162cc00b5af8c272722a0ac0ba
sdecoder <sde.work.inbox@gmail.com>
2016-08-06 02:13:34 -0400

    2016-08-06 02:13:34

--- bit/compute-modulus-division-by-a-power-of-2-number.cc
+++ bit/compute-modulus-division-by-a-power-of-2-number.cc
@@ -1,20 +1,17 @@
-#include<stdio.h>
- 
+#include <stdio.h>
+
 /* This function will return n % d.
    d must be one of: 1, 2, 4, 8, 16, 32, … */
-unsigned int getModulo(unsigned int n, unsigned int d)
-{
-  return ( n & (d-1) );
-}         
- 
+unsigned int getModulo(unsigned int n, unsigned int d) {
+  return (n & (d - 1));
+}
+
 /* Driver program to test above function */
-int main()
-{
+int main() {
   unsigned int n = 6;
   unsigned int d = 4; /*d must be a power of 2*/
   printf("%u moduo %u is %u", n, d, getModulo(n, d));
- 
+
   getchar();
   return 0;
-}     
-
+}
