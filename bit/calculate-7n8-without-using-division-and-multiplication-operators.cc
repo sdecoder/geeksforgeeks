7674f7dab5aa95c9fbdf12005f940f8e97268e98
sdecoder <sde.work.inbox@gmail.com>
2016-08-25 09:01:06 -0400

    2016-08-25 09:01:06

--- bit/calculate-7n8-without-using-division-and-multiplication-operators.cc
+++ bit/calculate-7n8-without-using-division-and-multiplication-operators.cc
@@ -1,19 +1,15 @@
 // C program to evaluate 7n/8 without using * and /
-#include<stdio.h>
- 
-int multiplyBySevenByEight(unsigned int n)
-{   
-    /* Step 1) First multiply number by 7 i.e. 7n = (n << 3) -n
-     * Step 2) Divide result by 8 */
-   return ((n << 3) -n) >> 3;
+#include <stdio.h>
+
+int multiplyBySevenByEight(unsigned int n) {
+  /* Step 1) First multiply number by 7 i.e. 7n = (n << 3) -n
+   * Step 2) Divide result by 8 */
+  return ((n << 3) - n) >> 3;
 }
- 
+
 /* Driver program to test above function */
-int main()
-{
-    unsigned int n = 15;
-    printf("%u", multiplyBySevenByEight(n));
-    return 0;
+int main() {
+  unsigned int n = 15;
+  printf("%u", multiplyBySevenByEight(n));
+  return 0;
 }
-
-
