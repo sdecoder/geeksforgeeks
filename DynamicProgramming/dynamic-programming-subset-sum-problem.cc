410f3adba7b6e329e884e9a873aa8f9254d75173
sdecoder <sde.work.inbox@gmail.com>
2016-09-18 12:12:42 -0400

    2016-09-18 12:12:42

--- DynamicProgramming/dynamic-programming-subset-sum-problem.cc
+++ DynamicProgramming/dynamic-programming-subset-sum-problem.cc
@@ -8,19 +8,16 @@ bool isSubsetSum(int set[], int n, int sum) {
   bool subset[sum + 1][n + 1];
 
   // If sum is 0, then answer is true
-  for (int i = 0; i <= n; i++)
-    subset[0][i] = true;
+  for (int i = 0; i <= n; i++) subset[0][i] = true;
 
   // If sum is not 0 and set is empty, then answer is false
-  for (int i = 1; i <= sum; i++)
-    subset[i][0] = false;
+  for (int i = 1; i <= sum; i++) subset[i][0] = false;
 
   // Fill the subset table in botton up manner
   for (int i = 1; i <= sum; i++) {
     for (int j = 1; j <= n; j++) {
       subset[i][j] = subset[i][j - 1];
-      if (i >= set[j - 1])
-        subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
+      if (i >= set[j - 1]) subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
     }
   }
 
