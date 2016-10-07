34dda5cecbfbecede06eb44078791f44e6b45c87
sdecoder <sde.work.inbox@gmail.com>
2016-10-07 07:46:07 -0400

    2016-10-07 07:46:07

--- bit/rotate-bits-of-an-integer.cc
+++ bit/rotate-bits-of-an-integer.cc
@@ -1,25 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #define INT_BITS 32
- 
+
 /*Function to left rotate n by d bits*/
-int leftRotate(int n, unsigned int d)
-{
-   /* In n<<d, last d bits are 0. To put first 3 bits of n at 
-     last, do bitwise or of n<<d with n >>(INT_BITS - d) */
-   return (n << d)|(n >> (INT_BITS - d));
+int leftRotate(int n, unsigned int d) {
+  /* In n<<d, last d bits are 0. To put first 3 bits of n at
+    last, do bitwise or of n<<d with n >>(INT_BITS - d) */
+  return (n << d) | (n >> (INT_BITS - d));
 }
- 
+
 /*Function to right rotate n by d bits*/
-int rightRotate(int n, unsigned int d)
-{
-   /* In n>>d, first d bits are 0. To put last 3 bits of at 
-     first, do bitwise or of n>>d with n <<(INT_BITS - d) */
-   return (n >> d)|(n << (INT_BITS - d));
+int rightRotate(int n, unsigned int d) {
+  /* In n>>d, first d bits are 0. To put last 3 bits of at
+    first, do bitwise or of n>>d with n <<(INT_BITS - d) */
+  return (n >> d) | (n << (INT_BITS - d));
 }
- 
+
 /* Driver program to test above functions */
-int main()
-{
+int main() {
   int n = 16;
   int d = 2;
   printf("Left Rotation of %d by %d is ", n, d);
@@ -27,6 +24,4 @@ int main()
   printf("\nRight Rotation of %d by %d is ", n, d);
   printf("%d", rightRotate(n, d));
   getchar();
-} 
-
-
+}
