8104de72d4f14a39e4f2d19ddf75273b42fa2334
sdecoder <sde.work.inbox@gmail.com>
2016-10-13 18:13:48 -0400

    2016-10-13 18:13:48

--- DynamicProgramming/dynamic-programming-set-14-maximum-sum-increasing-subsequence.cc
+++ DynamicProgramming/dynamic-programming-set-14-maximum-sum-increasing-subsequence.cc
@@ -9,19 +9,16 @@ int maxSumIS(int arr[], int n) {
   int msis[n];
 
   /* Initialize msis values for all indexes */
-  for (i = 0; i < n; i++)
-    msis[i] = arr[i];
+  for (i = 0; i < n; i++) msis[i] = arr[i];
 
   /* Compute maximum sum values in bottom up manner */
   for (i = 1; i < n; i++)
     for (j = 0; j < i; j++)
-      if (arr[i] > arr[j] && msis[i] < msis[j] + arr[i])
-        msis[i] = msis[j] + arr[i];
+      if (arr[i] > arr[j] && msis[i] < msis[j] + arr[i]) msis[i] = msis[j] + arr[i];
 
   /* Pick maximum of all msis values */
   for (i = 0; i < n; i++)
-    if (max < msis[i])
-      max = msis[i];
+    if (max < msis[i]) max = msis[i];
 
   return max;
 }
