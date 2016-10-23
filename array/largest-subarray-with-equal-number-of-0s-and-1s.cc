9c9693a904ae880cec8d306138d1e4d80d9913e4
sdecoder <sde.work.inbox@gmail.com>
2016-10-23 01:39:23 -0400

    2016-10-23 01:39:23

--- array/largest-subarray-with-equal-number-of-0s-and-1s.cc
+++ array/largest-subarray-with-equal-number-of-0s-and-1s.cc
@@ -1,50 +1,44 @@
 // A simple program to find the largest subarray with equal number of 0s and 1s
 #include <stdio.h>
- 
+
 // This function Prints the starting and ending
-// indexes of the largest subarray with equal 
-// number of 0s and 1s. Also returns the size 
+// indexes of the largest subarray with equal
+// number of 0s and 1s. Also returns the size
 // of such subarray.
-int findSubArray(int arr[], int n)
-{
-    int sum = 0;
-    int maxsize = -1, startindex;
- 
-    // Pick a starting point as i
-    for (int i = 0; i < n-1; i++)
-    {
-        sum = (arr[i] == 0)? -1 : 1;
- 
-        // Consider all subarrays starting from i
-        for (int j = i+1; j < n; j++)
-        {
-            (arr[j] == 0)? sum += -1: sum += 1;
- 
-            // If this is a 0 sum subarray, then 
-            // compare it with maximum size subarray
-            // calculated so far
-            if (sum == 0 && maxsize < j-i+1)
-            {
-                maxsize = j - i + 1;
-                startindex = i;
-            }
-        }
+int findSubArray(int arr[], int n) {
+  int sum = 0;
+  int maxsize = -1, startindex;
+
+  // Pick a starting point as i
+  for (int i = 0; i < n - 1; i++) {
+    sum = (arr[i] == 0) ? -1 : 1;
+
+    // Consider all subarrays starting from i
+    for (int j = i + 1; j < n; j++) {
+      (arr[j] == 0) ? sum += -1 : sum += 1;
+
+      // If this is a 0 sum subarray, then
+      // compare it with maximum size subarray
+      // calculated so far
+      if (sum == 0 && maxsize < j - i + 1) {
+        maxsize = j - i + 1;
+        startindex = i;
+      }
     }
-    if (maxsize == -1)
-        printf("No such subarray");
-    else
-        printf("%d to %d", startindex, startindex+maxsize-1);
- 
-    return maxsize;
-}
- 
-/* Driver program to test above functions*/
-int main()
-{
-    int arr[] =  {1, 0, 0, 1, 0, 1, 1};
-    int size = sizeof(arr)/sizeof(arr[0]);
- 
-    findSubArray(arr, size);
-    return 0;
+  }
+  if (maxsize == -1)
+    printf("No such subarray");
+  else
+    printf("%d to %d", startindex, startindex + maxsize - 1);
+
+  return maxsize;
 }
 
+/* Driver program to test above functions*/
+int main() {
+  int arr[] = {1, 0, 0, 1, 0, 1, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  findSubArray(arr, size);
+  return 0;
+}
