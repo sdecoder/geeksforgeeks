748ce4da6e102dc7c6a01befb12af72731e6f1bc
sdecoder <sde.work.inbox@gmail.com>
2016-11-08 15:22:43 -0500

    2016-11-08 15:22:43

--- linked-list/decimal-equivalent-of-binary-linked-list.cc
+++ linked-list/decimal-equivalent-of-binary-linked-list.cc
@@ -1,55 +1,48 @@
 // C++ Program to find decimal value of
 // binary linked list
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /* Link list Node */
-struct Node
-{
-    bool data;
-    struct Node* next;
+struct Node {
+  bool data;
+  struct Node* next;
 };
 
 /* Returns decimal value of binary linked list */
-int decimalValue(struct Node *head)
-{
-    // Initialized result
-    int  res = 0;
+int decimalValue(struct Node* head) {
+  // Initialized result
+  int res = 0;
 
-    // Traverse linked list
-    while (head != NULL)
-    {
-        // Multiply result by 2 and add
-        // head's data
-        res = (res  << 1) + head->data;
+  // Traverse linked list
+  while (head != NULL) {
+    // Multiply result by 2 and add
+    // head's data
+    res = (res << 1) + head->data;
 
-        // Move next
-        head = head->next;
-    }
-    return res;
+    // Move next
+    head = head->next;
+  }
+  return res;
 }
 
 // Utility function to create a new node.
-Node *newNode(bool data)
-{
-    struct Node *temp = new Node;
-    temp->data = data;
-    temp->next = NULL;
-    return temp;
+Node* newNode(bool data) {
+  struct Node* temp = new Node;
+  temp->data = data;
+  temp->next = NULL;
+  return temp;
 }
 
 /* Drier program to test above function*/
-int main()
-{
-    /* Start with the empty list */
-    struct Node* head = newNode(1);
-    head->next = newNode(0);
-    head->next->next = newNode(1);
-    head->next->next->next = newNode(1);
+int main() {
+  /* Start with the empty list */
+  struct Node* head = newNode(1);
+  head->next = newNode(0);
+  head->next->next = newNode(1);
+  head->next->next->next = newNode(1);
 
-    cout << "Decimal value is "
-         << decimalValue(head);
+  cout << "Decimal value is " << decimalValue(head);
 
-    return 0;
+  return 0;
 }
-
