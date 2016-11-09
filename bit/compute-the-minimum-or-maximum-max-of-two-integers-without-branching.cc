d976b82f4d8d817d2ce90b568a28a3bb2396697b
sdecoder <sde.work.inbox@gmail.com>
2016-11-09 01:54:39 -0500

    2016-11-09 01:54:39

--- bit/compute-the-minimum-or-maximum-max-of-two-integers-without-branching.cc
+++ bit/compute-the-minimum-or-maximum-max-of-two-integers-without-branching.cc
@@ -1,23 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 #define CHAR_BIT 8
- 
+
 /*Function to find minimum of x and y*/
-int min(int x, int y)
-{
-  return  y + ((x - y) & ((x - y) >> 
-            (sizeof(int) * CHAR_BIT - 1))); 
+int min(int x, int y) {
+  return y + ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1)));
 }
- 
+
 /*Function to find maximum of x and y*/
-int max(int x, int y)
-{
-  return x - ((x - y) & ((x - y) >>
-            (sizeof(int) * CHAR_BIT - 1)));
+int max(int x, int y) {
+  return x - ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1)));
 }
- 
+
 /* Driver program to test above functions */
-int main()
-{
+int main() {
   int x = 15;
   int y = 6;
   printf("Minimum of %d and %d is ", x, y);
@@ -26,5 +21,3 @@ int main()
   printf("%d", max(x, y));
   getchar();
 }
-
-
