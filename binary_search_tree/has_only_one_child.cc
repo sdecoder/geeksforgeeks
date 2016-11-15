fafb517e0bf0f5e94697cd93e8a78e7d9c5f9b6d
sdecoder <sde.work.inbox@gmail.com>
2016-11-15 11:55:18 -0500

    2016-11-15 11:55:18

--- binary_search_tree/has_only_one_child.cc
+++ binary_search_tree/has_only_one_child.cc
@@ -1,42 +1,37 @@
 #include <stdio.h>
 
-int hasOnlyOneChild(int pre[], int size)
-{
-    // Initialize min and max using last two elements
-    int min, max;
-    if (pre[size-1] > pre[size-2])
-    {
-        max = pre[size-1];
+int hasOnlyOneChild(int pre[], int size) {
+  // Initialize min and max using last two elements
+  int min, max;
+  if (pre[size - 1] > pre[size - 2]) {
+    max = pre[size - 1];
         min = pre[size-2]):
     else
     {
-        max = pre[size-2];
-        min = pre[size-1];
-    }
+          max = pre[size - 2];
+          min = pre[size - 1];
+        }
 
-
-    // Every element must be either smaller than min or
-    // greater than max
-    for (int i=size-3; i>=0; i--)
-    {
-        if (pre[i] < min)
+        // Every element must be either smaller than min or
+        // greater than max
+        for (int i = size - 3; i >= 0; i--) {
+          if (pre[i] < min)
             min = pre[i];
-        else if (pre[i] > max)
+          else if (pre[i] > max)
             max = pre[i];
-        else
+          else
             return false;
-    }
-    return true;
-}
+        }
+        return true;
+  }
 
-// Driver program to test above function
-int main()
-{
+  // Driver program to test above function
+  int main() {
     int pre[] = {8, 3, 5, 7, 6};
-    int size = sizeof(pre)/sizeof(pre[0]);
-    if (hasOnlyOneChild(pre,size))
-        printf("Yes");
+    int size = sizeof(pre) / sizeof(pre[0]);
+    if (hasOnlyOneChild(pre, size))
+      printf("Yes");
     else
-        printf("No");
+      printf("No");
     return 0;
-}
+  }
