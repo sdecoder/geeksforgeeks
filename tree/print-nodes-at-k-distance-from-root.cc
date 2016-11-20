15209a4fe915676ff04e242a68a0268831a5d259
sdecoder <sde.work.inbox@gmail.com>
2016-11-20 08:16:20 -0500

    2016-11-20 08:16:20

--- tree/print-nodes-at-k-distance-from-root.cc
+++ tree/print-nodes-at-k-distance-from-root.cc
@@ -1,66 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 /* A binary tree node has data, pointer to left child
    and a pointer to right child */
-struct node
-{
-   int data;
-   struct node* left;
-   struct node* right;
+struct node {
+  int data;
+  struct node* left;
+  struct node* right;
 };
- 
-void printKDistant(node *root , int k)    
-{
-   if(root == NULL) 
-      return;
-   if( k == 0 )
-   {
-      printf( "%d ", root->data );
-      return ;
-   }
-   else
-   {      
-      printKDistant( root->left, k-1 ) ;
-      printKDistant( root->right, k-1 ) ;
-   }
+
+void printKDistant(node* root, int k) {
+  if (root == NULL) return;
+  if (k == 0) {
+    printf("%d ", root->data);
+    return;
+  } else {
+    printKDistant(root->left, k - 1);
+    printKDistant(root->right, k - 1);
+  }
 }
- 
+
 /* Helper function that allocates a new node with the
    given data and NULL left and right pointers. */
-struct node* newNode(int data)
-{
-  struct node* node = (struct node*)
-                       malloc(sizeof(struct node));
+struct node* newNode(int data) {
+  struct node* node = (struct node*)malloc(sizeof(struct node));
   node->data = data;
   node->left = NULL;
   node->right = NULL;
- 
-  return(node);
+
+  return (node);
 }
- 
+
 /* Driver program to test above functions*/
-int main()
-{
- 
+int main() {
   /* Constructed binary tree is
             1
           /   \
         2      3
       /  \    /
-    4     5  8 
+    4     5  8
   */
-  struct node *root = newNode(1);
-  root->left        = newNode(2);
-  root->right       = newNode(3);
-  root->left->left  = newNode(4);
+  struct node* root = newNode(1);
+  root->left = newNode(2);
+  root->right = newNode(3);
+  root->left->left = newNode(4);
   root->left->right = newNode(5);
-  root->right->left = newNode(8);  
- 
+  root->right->left = newNode(8);
+
   printKDistant(root, 2);
- 
+
   getchar();
   return 0;
 }
-
-
