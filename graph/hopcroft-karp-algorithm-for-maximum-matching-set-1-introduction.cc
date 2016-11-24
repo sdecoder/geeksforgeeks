ed31c4c750f749773c9636d3cbd0648452b3d650
sdecoder <sde.work.inbox@gmail.com>
2016-11-23 20:40:32 -0500

    2016-11-23 20:40:32

--- graph/hopcroft-karp-algorithm-for-maximum-matching-set-1-introduction.cc
+++ graph/hopcroft-karp-algorithm-for-maximum-matching-set-1-introduction.cc
@@ -1,4 +1,7 @@
 /*
-A matching in a Bipartite Graph is a set of the edges chosen in such a way that no two edges share an endpoint. A maximum matching is a matching of maximum size (maximum number of edges). In a maximum matching, if any edge is added to it, it is no longer a matching. There can be more than one maximum matching for a given Bipartite Graph.
+A matching in a Bipartite Graph is a set of the edges chosen in such a way that no two
+edges share an endpoint. A maximum matching is a matching of maximum size (maximum number
+of edges). In a maximum matching, if any edge is added to it, it is no longer a matching.
+There can be more than one maximum matching for a given Bipartite Graph.
 
 */
