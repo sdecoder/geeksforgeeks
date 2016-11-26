b75d56cf27d0437577f4c0e8f7a7a4118ad066f9
sdecoder <sde.work.inbox@gmail.com>
2016-11-26 07:42:45 -0500

    2016-11-26 07:42:45

--- bit/calculate-square-of-a-number-without-using-and-pow.cc
+++ bit/calculate-square-of-a-number-without-using-and-pow.cc
@@ -1,31 +1,26 @@
 // Square of a number using bitwise operators
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-int square(int n)
-{
-    // Base case
-    if (n==0) return 0;
- 
-    // Handle negative number
-    if (n < 0) n = -n;
- 
-    // Get floor(n/2) using right shift
-    int x = n>>1;
- 
-    // If n is odd
-    if (n&1)
-        return ((square(x)<<2) + (x<<2) + 1);
-    else // If n is even
-        return (square(x)<<2);
+
+int square(int n) {
+  // Base case
+  if (n == 0) return 0;
+
+  // Handle negative number
+  if (n < 0) n = -n;
+
+  // Get floor(n/2) using right shift
+  int x = n >> 1;
+
+  // If n is odd
+  if (n & 1)
+    return ((square(x) << 2) + (x << 2) + 1);
+  else // If n is even
+    return (square(x) << 2);
 }
- 
+
 // drive program
-int main()
-{
-    for (int n = 1; n<=5; n++)
-        cout << "n = " << n << ", n^2 = " << square(n) << endl;
-    return 0;
+int main() {
+  for (int n = 1; n <= 5; n++) cout << "n = " << n << ", n^2 = " << square(n) << endl;
+  return 0;
 }
-
-
