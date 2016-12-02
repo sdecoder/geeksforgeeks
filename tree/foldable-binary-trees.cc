39ae42445afe2c4faa9eefb1792aa94d5f0fa812
sdecoder <sde.work.inbox@gmail.com>
2016-12-02 03:16:23 -0500

    2016-12-02 03:16:23

---
+++ tree/foldable-binary-trees.cc
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+/* You would want to remove below 3 lines if your compiler
+   supports bool, true and false */
+#define bool int
+#define true 1
+#define false 0
+ 
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct node
+{
+  int data;
+  struct node* left;
+  struct node* right;
+};
+ 
+/* converts a tree to its mrror image */
+void mirror(struct node* node);
+ 
+/* returns true if structure of two trees a and b is same
+   Only structure is considered for comparison, not data! */
+bool isStructSame(struct node *a, struct node *b);
+ 
+/* Returns true if the given tree is foldable */
+bool isFoldable(struct node *root)
+{
+  bool res;
+ 
+  /* base case */
+  if(root == NULL)
+    return true;
+ 
+  /* convert left subtree to its mirror */
+  mirror(root->left);
+ 
+  /* Compare the structures of the right subtree and mirrored
+    left subtree */
+  res = isStructSame(root->left, root->right);
+ 
+  /* Get the originial tree back */
+  mirror(root->left);
+ 
+  return res;
+}
+ 
+ 
+bool isStructSame(struct node *a, struct node *b)
+{
+  if (a == NULL && b == NULL)
+  {  return true; }
+  if ( a != NULL && b != NULL &&
+       isStructSame(a->left, b->left) &&
+       isStructSame(a->right, b->right)
+     )
+  {  return true; }
+ 
+  return false;
+}
+ 
+ 
+/* UTILITY FUNCTIONS */
+/* Change a tree so that the roles of the  left and
+    right pointers are swapped at every node.
+    See http://geeksforgeeks.org/?p=662 for details */
+void mirror(struct node* node)
+{
+  if (node==NULL)
+    return;
+  else
+  {
+    struct node* temp;
+ 
+    /* do the subtrees */
+    mirror(node->left);
+    mirror(node->right);
+ 
+    /* swap the pointers in this node */
+    temp        = node->left;
+    node->left  = node->right;
+    node->right = temp;
+  }
+}
+ 
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct node* newNode(int data)
+{
+  struct node* node = (struct node*)
+                       malloc(sizeof(struct node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+ 
+  return(node);
+}
+ 
+/* Driver program to test mirror() */
+int main(void)
+{
+  /* The constructed binary tree is
+         1
+       /   \
+      2     3
+      \    /
+       4  5
+  */
+  struct node *root = newNode(1);
+  root->left        = newNode(2);
+  root->right       = newNode(3);
+  root->right->left  = newNode(4);
+  root->left->right = newNode(5);
+ 
+  if(isFoldable(root) == 1)
+  { printf("\n tree is foldable"); }
+  else
+  { printf("\n tree is not foldable"); }
+ 
+  getchar();
+  return 0;
+}
+
+
