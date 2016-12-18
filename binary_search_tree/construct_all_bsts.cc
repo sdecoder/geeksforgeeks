f4d7c50cd31d843bf9e611a913516ffb76f92a1a
sdecoder <sde.work.inbox@gmail.com>
2016-12-18 15:54:17 -0500

    2016-12-18 15:54:17

--- binary_search_tree/construct_all_bsts.cc
+++ binary_search_tree/construct_all_bsts.cc
@@ -6,19 +6,19 @@ using namespace std;
 //  node structure
 struct node {
   int key;
-  struct node *left, *right;
+  struct node* left, *right;
 };
 
 // A utility function to create a new BST node
-struct node *newNode(int item) {
-  struct node *temp = new node;
+struct node* newNode(int item) {
+  struct node* temp = new node;
   temp->key = item;
   temp->left = temp->right = NULL;
   return temp;
 }
 
 // A utility function to do preorder traversal of BST
-void preorder(struct node *root) {
+void preorder(struct node* root) {
   if (root != NULL) {
     cout << root->key << " ";
     preorder(root->left);
@@ -27,8 +27,8 @@ void preorder(struct node *root) {
 }
 
 //  function for constructing trees
-vector<struct node *> constructTrees(int start, int end) {
-  vector<struct node *> list;
+vector<struct node*> constructTrees(int start, int end) {
+  vector<struct node*> list;
 
   /*  if start > end   then subtree will be empty so returning NULL
       in the list */
@@ -41,21 +41,21 @@ vector<struct node *> constructTrees(int start, int end) {
       left and right subtree recursively  */
   for (int i = start; i <= end; i++) {
     /*  constructing left subtree   */
-    vector<struct node *> leftSubtree = constructTrees(start, i - 1);
+    vector<struct node*> leftSubtree = constructTrees(start, i - 1);
 
     /*  constructing right subtree  */
-    vector<struct node *> rightSubtree = constructTrees(i + 1, end);
+    vector<struct node*> rightSubtree = constructTrees(i + 1, end);
 
     /*  now looping through all left and right subtrees and connecting
         them to ith root  below  */
     for (int j = 0; j < leftSubtree.size(); j++) {
-      struct node *left = leftSubtree[j];
+      struct node* left = leftSubtree[j];
       for (int k = 0; k < rightSubtree.size(); k++) {
-        struct node *right = rightSubtree[k];
-        struct node *node = newNode(i);  // making value i as root
-        node->left = left;               // connect left subtree
-        node->right = right;             // connect right subtree
-        list.push_back(node);            // add this tree to list
+        struct node* right = rightSubtree[k];
+        struct node* node = newNode(i); // making value i as root
+        node->left = left;              // connect left subtree
+        node->right = right;            // connect right subtree
+        list.push_back(node);           // add this tree to list
       }
     }
   }
@@ -65,7 +65,7 @@ vector<struct node *> constructTrees(int start, int end) {
 // Driver Program to test above functions
 int main() {
   // Construct all possible BSTs
-  vector<struct node *> totalTreesFrom1toN = constructTrees(1, 3);
+  vector<struct node*> totalTreesFrom1toN = constructTrees(1, 3);
 
   /*  Printing preorder traversal of all constructed BSTs   */
   cout << "Preorder traversals of all constructed BSTs are \n";
