95a8fa810b0cdc82fd2864fc1d98d812536d97af
sdecoder <sde.work.inbox@gmail.com>
2016-12-20 13:45:02 -0500

    2016-12-20 13:45:02

--- array/given-binary-string-count-number-substrings-start-end-1.cc
+++ array/given-binary-string-count-number-substrings-start-end-1.cc
@@ -1,29 +1,23 @@
 // A O(n) C++ program to count number of substrings starting and ending
 // with 1
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-int countSubStr(char str[])
-{
-   int m = 0; // Count of 1's in input string
- 
-   // Travers input string and count of 1's in it
-   for (int i=0; str[i] !='\0'; i++)
-   {
-        if (str[i] == '1')
-           m++;
-   }
- 
-   // Return count of possible pairs among m 1's
-   return m*(m-1)/2;
+
+int countSubStr(char str[]) {
+  int m = 0; // Count of 1's in input string
+
+  // Travers input string and count of 1's in it
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] == '1') m++;
+  }
+
+  // Return count of possible pairs among m 1's
+  return m * (m - 1) / 2;
 }
- 
+
 // Driver program to test above function
-int main()
-{
+int main() {
   char str[] = "00100101";
   cout << countSubStr(str);
   return 0;
 }
-
-
